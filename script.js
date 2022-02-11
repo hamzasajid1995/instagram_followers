2387ca16f7058e51a0e1a3a580517846981ea404
Hamza sajid <hamzasajid424@gmail.com>
2022-02-11 23:08:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-// copy fetch request from your account
-fetch(
-  "https://i.instagram.com/api/v1/friendships/<__some_id__>/followers/",
-  {
-    headers: {
-      accept: "*/*",
-      "accept-language": "en-US,en;q=0.9",
-      "sec-ch-ua":
-        '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"macOS"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
-    },
-    referrer: "https://www.instagram.com/",
-    referrerPolicy: "strict-origin-when-cross-origin",
-    body: null,
-    method: "GET",
-    mode: "cors",
-    credentials: "include"
-  }
-)
+// Save previous followers
+var prevFollowers = [];
+
+// Copy fetch request from your account (Dev tools > network)
+fetch("https://i.instagram.com/api/v1/friendships/<____>/followers", {
+  headers: {
+    accept: "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-ch-ua":
+      '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+  },
+  referrer: "https://www.instagram.com/",
+  referrerPolicy: "strict-origin-when-cross-origin",
+  body: null,
+  method: "GET",
+  mode: "cors",
+  credentials: "include",
+})
   .then((res) => res.json())
   .then((res) => {
-    let newFollowers = res.users.map((item) => item.username);
-    console.log(newFollowers)
-    findDiff(newFollowers);
+    let followers = res.users.map((item) => item.username);
+    console.log({ followers });
+    findDiff(followers);
   });
 
-var prevFollowers = []; // save your prev followers localy
-
 function findDiff(newFollowers) {
-  let removed = prevFollowers.filter(obj => (newFollowers.indexOf(obj) == -1) );
-  let addded = newFollowers.filter(obj => prevFollowers.indexOf(obj) == -1));
+  let removed = prevFollowers.filter((obj) => newFollowers.indexOf(obj) == -1);
+  let added = newFollowers.filter((obj) => prevFollowers.indexOf(obj) == -1);
   console.log({ removed, added });
 }
